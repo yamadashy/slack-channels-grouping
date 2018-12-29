60e25b597a14180aab2251c67aedefbf8f613288
Kazuki Yamada <kaz-yamada@colopl.co.jp>
2018-12-29 14:02:17 +0900

    チャンネル一覧を監視

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,17 +2,17 @@ import $ from "jquery";
 
 class ChannelGrouper {
     constructor() {
-        this.$channelItems = $(".p-channel_sidebar__static_list [role=listitem]");
     }
 
     groupingAllByPrefix() {
-        var previousPrefix = "";
+        const channelItems = $(".p-channel_sidebar__static_list [role=listitem]");
+        let previousPrefix = "";
         let prefixes = [];
 
-        this.$channelItems.each(function (index, elem) {
-            var $item = $(elem)
-            var $span = $item.find("a > span");
-            var channelName = $.trim($span.text());
+        channelItems.each(function (index, elem) {
+            const $item = $(elem)
+            const $span = $item.find("a > span");
+            const channelName = $.trim($span.text());
 
             if (/^.+?[-_].*/.test(channelName)) {
                 const prefix = channelName.match(/(^.+?)[-_].*/)[1];
@@ -20,32 +20,48 @@ class ChannelGrouper {
             }
         });
 
-        this.$channelItems.each(function (index, elem) {
-            var $item = $(elem)
-            var $span = $item.find("a > span");
-            var channelName = $.trim($span.text());
+        channelItems.each(function (index, elem) {
+            const $item = $(elem)
+            const $span = $item.find("a > span");
+            const channelName = $.trim($span.text());
+            const prefix = prefixes[index];
+            const isLoneliness = prefixes[index - 1] !== prefix && prefixes[index + 1] !== prefix;
 
-            if (/^.+?[-_].*/.test(channelName)) {
-                const prefix = prefixes[index];
-                const isLoneliness = prefixes[index - 1] !== prefix && prefixes[index + 1] !== prefix;
-
-                if (isLoneliness) {
-                    return;
-                }
-
-                const isParent = prefix !== previousPrefix;
-                const isLastChild = prefixes[index + 1] !== prefix;
-                const separator = isParent ? "┬" : (isLastChild ? "└" : "├");
-
-                $span
-                    .empty()
-                    .addClass(isParent ? "scg-ch-parent" : "scg-ch-child")
-                    .append($("<span>").addClass("scg-ch-prefix").text(prefix))
-                    .append($("<span>").addClass("scg-ch-separator").text(separator))
-                    .append($("<span>").addClass("scg-ch-name").text(channelName.replace(/(^.+?)[-_](.*)/, "$2")));
-
-                previousPrefix = prefix;
+            if ($span.hasClass("scg-ch-parent") || $span.hasClass("scg-ch-child")) {
+                return;
             }
+
+            if (isLoneliness) {
+                return;
+            }
+
+            if (prefixes[index] == null) {
+                return;
+            }
+
+            const isParent = prefix !== previousPrefix;
+            const isLastChild = prefixes[index + 1] !== prefix;
+            const separator = isParent ? "┬" : (isLastChild ? "└" : "├");
+
+            $span
+                .empty()
+                .addClass(isParent ? "scg-ch-parent" : "scg-ch-child")
+                .append($("<span>").addClass("scg-ch-prefix").text(prefix))
+                .append($("<span>").addClass("scg-ch-separator").text(separator))
+                .append($("<span>").addClass("scg-ch-name").text(channelName.replace(/(^.+?)[-_](.*)/, "$2")));
+
+            previousPrefix = prefix;
+        });
+    }
+
+    watchChannelList() {
+        const watchTarget = document.querySelector(".p-channel_sidebar__static_list");
+        const observer = new MutationObserver((mutations) => {
+            this.groupingAllByPrefix();
+        });
+
+        observer.observe(watchTarget, {
+            childList: true,
         });
     }
 }
@@ -53,4 +69,5 @@ class ChannelGrouper {
 setTimeout(function () {
     const channelGrouper = new ChannelGrouper();
     channelGrouper.groupingAllByPrefix();
+    channelGrouper.watchChannelList();
 }, 3000);
