194cd6aaa3e5e9c0f2590862f3d8225d92cdb89a
Kazuki Yamada <kaz-yamada@colopl.co.jp>
2019-09-02 01:13:15 +0900

    change: babel setting

--- webextension-toolbox-config.js
+++ webextension-toolbox-config.js
@@ -14,6 +14,21 @@ module.exports = {
       test: /\.ts$/,
       loader: 'ts-loader'
     })
+    config.module.rules.push({
+      test: /\.js$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true,
+            extends: path.join(__dirname + '/.babelrc'),
+            exclude: /node_modules/,
+            cacheDirectory: true,
+            envName: dev ? 'development' : 'production'
+          }
+        }
+      ]
+    })
 
     return config
   },
