939fcca57fa01843714aba91c3dd15a9ffd12967
Kazuki Yamada <koukun0120@gmail.com>
2020-02-13 01:24:57 +0900

    update: split scripts

---
+++ app/scripts/slack/selectors.ts
@@ -0,0 +1,5 @@
+const CHANNEL_LIST_SELECTOR = '.p-channel_sidebar__static_list';
+
+export const CHANNEL_LIST_CONTAINER_SELECTOR = CHANNEL_LIST_SELECTOR + ' .c-virtual_list__scroll_container';
+export const CHANNEL_LIST_ITEMS_SELECTOR = CHANNEL_LIST_SELECTOR + ' [role=listitem]';
+export const CHANNEL_NAME_SELECTOR = '.p-channel_sidebar__name';
