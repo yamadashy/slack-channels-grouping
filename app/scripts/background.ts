7f73ecb27f67b2ec63246ca11dce8c8ad05bd707
Kazuki Yamada <yamadashy@yamaMac.local>
2020-05-09 22:17:14 +0900

    update: auto apply scripts on install

---
+++ app/scripts/background.ts
@@ -0,0 +1,26 @@
+function updateExtensionContents(): void {
+  chrome.tabs.query({}, (tabs) => {
+    for (const tabKey in tabs) {
+      const tab = tabs[tabKey];
+
+      // Do not have permission
+      if (tab.url === undefined) {
+        continue;
+      }
+
+      chrome.tabs.insertCSS(tab.id, {
+        file: 'styles/content.css',
+      }, () => {
+        chrome.tabs.executeScript(tab.id, {
+          file: 'scripts/vendor.js',
+        }, () => {
+          chrome.tabs.executeScript(tab.id, {
+            file: 'scripts/content.js',
+          });
+        });
+      });
+    }
+  });
+}
+
+updateExtensionContents();
--- app/scripts/content.ts
+++ app/scripts/content.ts
@@ -1,7 +1,18 @@
 import ChannelObserver from './slack/channel-observer';
 import ChannelGrouper from './slack/channe-grouper';
+import { BODY_EXTENSION_APPLIED_FLAG } from './slack/dom-constants';
 
 ((): void => {
+  // Check applied
+  const alreadyApplied = document.body.getAttribute(BODY_EXTENSION_APPLIED_FLAG) === '1';
+
+  if (alreadyApplied) {
+    return;
+  }
+
+  document.body.setAttribute(BODY_EXTENSION_APPLIED_FLAG, '1');
+
+  // Apply
   const channelObserver = new ChannelObserver();
   const channelGrouper = new ChannelGrouper();
   channelObserver.on('update', () => {
