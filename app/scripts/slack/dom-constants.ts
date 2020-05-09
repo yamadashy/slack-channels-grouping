b587243cd29d431bf179e0c2248b4a34f8bf68f9
Kazuki Yamada <yamadashy@yamaMac.local>
2020-05-09 23:20:02 +0900

    fix: dom constant for firefox

--- app/scripts/slack/dom-constants.ts
+++ app/scripts/slack/dom-constants.ts
@@ -2,7 +2,7 @@ const CHANNEL_LIST_SELECTOR = '.p-channel_sidebar__static_list';
 
 // selectors
 export const CHANNEL_LIST_CONTAINER_SELECTOR = CHANNEL_LIST_SELECTOR + ' .c-virtual_list__scroll_container';
-export const CHANNEL_LIST_ITEMS_SELECTOR = CHANNEL_LIST_SELECTOR + ' [role=listitem]';
+export const CHANNEL_LIST_ITEMS_SELECTOR = CHANNEL_LIST_SELECTOR + ' [role=listitem], ' + CHANNEL_LIST_SELECTOR + ' [role=treeitem]';
 export const CHANNEL_ITEM_CONTENTS_CONTAINER = '.p-channel_sidebar__channel';
 export const CHANNEL_ITEM_NAME_SELECTOR = '.p-channel_sidebar__name';
 
