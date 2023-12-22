3112942328f24580cfbafa21a26e8e0893f1464a
Kazuki Yamada <koukun0120@gmail.com>
2023-12-22 23:02:49 +0900

    chore: update: node version to 18 and packages

--- webextension-toolbox.config.js
+++ webextension-toolbox.config.js
@@ -3,52 +3,16 @@
 const path = require('path');
 const GlobEntriesPlugin = require('webpack-watched-glob-entries-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 
 module.exports = {
   webpack: (config, { dev, vendor }) => {
     const envName = dev ? 'development' : 'production';
 
-    // Support TypeScript
-    config.resolve.extensions.push('.ts');
-    config.entry = GlobEntriesPlugin.getEntries(
-      [
-        path.resolve('app', '*.{js,mjs,jsx,ts}'),
-        path.resolve('app', '?(scripts)/*.{js,mjs,jsx,ts}')
-      ]
-    );
-
-    // Loader
-    config.module.rules.push({
-      test: /\.ts$/,
-      loader: 'babel-loader',
-      options: {
-        cacheDirectory: true
-      }
-    });
-
     // Definitions
     config.plugins.push(new DefinePlugin({
       IS_PRODUCTION_BUILD: !dev,
     }))
-
-    // Runs typescript type checker on a separate process.
-    config.plugins.push(new ForkTsCheckerWebpackPlugin({
-      async: !!dev,
-      eslint: {
-        enable: !!dev,
-        files: '**/*.ts',
-        options: {
-          extensions: ['ts'],
-          cache: true,
-        }
-      },
-      typescript: {
-        enabled: true,
-        configFile: path.resolve(__dirname, 'tsconfig.json'),
-      },
-    }));
 
     // BundleAnalyzer
     config.plugins.push(new BundleAnalyzerPlugin({
