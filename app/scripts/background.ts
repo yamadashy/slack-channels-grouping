3e63b1c7689abf356885c589d194b6c010cc1f25
Kazuki Yamada <koukun0120@gmail.com>
2024-05-19 01:39:52 +0900

    update: catch inject error

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,27 +1,24 @@
-// Update extension content for slack tabs
-chrome.tabs.query({}, async (tabs) => {
-  for (const tabKey in tabs) {
-    const tab = tabs[tabKey];
+const injectContentToTab = async (tab: chrome.tabs.Tab) => {
+  // Do not have permission
+  if (tab.url === undefined) {
+    return;
+  }
 
-    // Do not have permission
-    if (tab.url === undefined) {
-      continue;
-    }
+  // Skip execute on discarded tab
+  if (tab.discarded) {
+    return;
+  }
 
-    // Skip execute on discarded tab
-    if (tab.discarded) {
-      continue;
-    }
+  // Under some circumstances a Tab may not be assigned an ID
+  if (tab.id === undefined) {
+    return;
+  }
 
-    // Under some circumstances a Tab may not be assigned an ID
-    if (tab.id === undefined) {
-      continue;
-    }
+  const manifest = chrome.runtime.getManifest();
+  const cssFiles = manifest.content_scripts?.[0].css ?? [];
+  const jsFiles = manifest.content_scripts?.[0].js ?? [];
 
-    const manifest = chrome.runtime.getManifest();
-    const cssFiles = manifest.content_scripts?.[0].css ?? [];
-    const jsFiles = manifest.content_scripts?.[0].js ?? [];
-
+  if (cssFiles.length > 0) {
     await chrome.scripting.insertCSS({
       target: {
         tabId: tab.id,
@@ -29,12 +26,27 @@ chrome.tabs.query({}, async (tabs) => {
       },
       files: cssFiles,
     });
-    chrome.scripting.executeScript({
+  }
+  if (jsFiles.length > 0) {
+    await chrome.scripting.executeScript({
       target: {
         tabId: tab.id,
         allFrames: true,
       },
       files: jsFiles,
     });
+  }
+};
+
+// Update extension content for tabs
+chrome.tabs.query({}, async (tabs) => {
+  for (const tabKey in tabs) {
+    const tab = tabs[tabKey];
+
+    try {
+      injectContentToTab(tab);
+    } catch (e) {
+      console.error(e);
+    }
   }
 });
